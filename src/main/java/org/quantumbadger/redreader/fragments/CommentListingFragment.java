d27acb9a0a6e2aaed066c4032ce51627d8576a29
Veeti Paananen <veeti.paananen@rojekti.fi>
2013-04-09 14:21:30 +0300

    Add a copy (to clipboard) action for links

--- src/main/java/org/quantumbadger/redreader/fragments/CommentListingFragment.java
+++ src/main/java/org/quantumbadger/redreader/fragments/CommentListingFragment.java
@@ -529,6 +529,7 @@ public class CommentListingFragment extends Fragment implements ActiveTextView.O
 				menu.add(R.string.action_gotosubreddit).setOnMenuItemClickListener(new MenuHandler(RedditPostView.Action.GOTO_SUBREDDIT));
 				menu.add(R.string.action_external).setOnMenuItemClickListener(new MenuHandler(RedditPostView.Action.EXTERNAL));
 				menu.add(R.string.action_share).setOnMenuItemClickListener(new MenuHandler(RedditPostView.Action.SHARE));
+				menu.add(R.string.action_copy).setOnMenuItemClickListener(new MenuHandler(RedditPostView.Action.COPY));
 				menu.add(R.string.action_share_comments).setOnMenuItemClickListener(new MenuHandler(RedditPostView.Action.SHARE_COMMENTS));
 
 				if(post.src.selftext != null && post.src.selftext.length() > 1) menu.add("Links in Self Text").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {

df6306542a94a624c2b27a3b6f184de7d0460ae6
QuantumBadger <git@redreader.org>
2013-07-03 12:01:26 +0100

    Further work on new markdown parser

--- src/main/java/org/quantumbadger/redreader/reddit/prepared/MarkdownParser.java
+++ src/main/java/org/quantumbadger/redreader/reddit/prepared/MarkdownParser.java
@@ -17,6 +17,13 @@
 
 package org.quantumbadger.redreader.reddit.prepared;
 
+import android.graphics.Typeface;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.SpannedString;
+import android.text.style.StyleSpan;
+import android.text.style.TypefaceSpan;
+
 import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -73,6 +80,7 @@ public final class MarkdownParser {
 					}
 					break;
 
+				// TODO multiple quote levels
 				case '>':
 					if(rawWithPrefix.charAt(1) == ' ') {
 						return new MarkdownParagraph(rawWithPrefix.substring(2), parent, Type.QUOTE);
@@ -129,8 +137,7 @@ public final class MarkdownParser {
 			return new MarkdownParagraph(rawWithPrefix, parent, Type.TEXT);
 		}
 
-
-		private MarkdownParagraph(String raw, MarkdownParagraph parent, Type type, int number) {
+		private MarkdownParagraph(final String raw, final MarkdownParagraph parent, final Type type, final int number) {
 			this.raw = raw.replace('\n', ' ');
 			this.parent = parent;
 			this.type = type;
@@ -139,6 +146,172 @@ public final class MarkdownParser {
 
 		private MarkdownParagraph(String raw, MarkdownParagraph parent, Type type) {
 			this(raw, parent, type, 0);
+		}
+
+		public float getPaddingDp() {
+
+			// TODO switch on type, parent type
+			// TODO detect two spaces at end of raw
+
+			return 10;
+
+		}
+
+		// TODO detect links in code
+		public Spanned generatedSpanned() {
+
+			if(type == Type.CODE) {
+				final SpannableStringBuilder builder = new SpannableStringBuilder(raw);
+				builder.setSpan(new TypefaceSpan("VeraMono"), 0, raw.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+				return builder;
+			}
+
+			// TODO output resized raw for headers
+
+			final SpannableStringBuilder builder = new SpannableStringBuilder();
+			final char[] rawArr = raw.toCharArray();
+
+			// TODO ensure these start at builder.length(), not i
+			int boldStart = -1, italicStart = -1, strikeStart = -1, linkStart = -1, codeStart = -1;
+
+			for(int i = 0; i < rawArr.length; i++) {
+
+				final char c = rawArr[i];
+
+				switch(c) {
+
+					case '_':
+					case '*':
+
+						if(i < rawArr.length - 1 && rawArr[i + 1] == c) {
+							// Double: bold
+
+							i++;
+
+							if(boldStart == -1) {
+
+								if(indexOf(rawArr, new char[]{c, c}, i + 1) != -1) {
+									boldStart = builder.length();
+
+								} else {
+									builder.append(c);
+									builder.append(c);
+								}
+
+							} else {
+								builder.setSpan(new StyleSpan(Typeface.BOLD), boldStart, builder.length(),
+										Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+								italicStart = -1;
+							}
+
+						} else {
+							// Single: italic
+
+							if(italicStart == -1) {
+
+								if(indexOf(rawArr, c, i + 1) != -1) {
+									italicStart = builder.length();
+
+								} else {
+									builder.append(c);
+								}
+
+							} else {
+								builder.setSpan(new StyleSpan(Typeface.ITALIC), italicStart, builder.length(),
+										Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+								italicStart = -1;
+							}
+
+						}
+
+						break;
+
+					case '~':
+						// TODO
+						break;
+
+					case '`':
+						// TODO just add raw until closing `
+						break;
+
+					case '[':
+
+						boolean linkSuccess = false;
+
+						if(linkStart == -1) {
+
+							final int closePos = indexOf(rawArr, ']', i + 1);
+							if(closePos != -1) {
+
+								final int urlOpenPos = indexOf(rawArr, '(', closePos + 1);
+								if(urlOpenPos != -1) {
+
+									if(isSpaces(rawArr, closePos + 1, urlOpenPos - closePos - 1)) {
+
+										final int urlClosePos = indexOf(rawArr, ')', urlOpenPos + 1);
+										if(urlClosePos != -1) {
+
+											if(indexOf(rawArr, '[', i + 1, closePos) == -1) {
+												linkStart = builder.length();
+												linkSuccess = true;
+											}
+										}
+									}
+								}
+							}
+						} else throw new RuntimeException("Internal error: unexpected '[' during parse");
+
+						if(!linkSuccess) builder.append('[');
+						break;
+
+					case ']':
+						// TODO
+						break;
+
+					case '\\':
+						// TODO
+						break;
+				}
+			}
+
+			return builder;
+		}
+
+		private boolean isSpaces(final char[] haystack, final int start, final int len) {
+			final int end = start + len;
+			for(int i = start; i < end; i++) {
+				if(haystack[i] != ' ') return false;
+			}
+			return true;
+		}
+
+		private int indexOf(char[] haystack, char needle, int fromIndexInclusive, int endIndexExclusive) {
+			for(int i = fromIndexInclusive; i < Math.min(endIndexExclusive, haystack.length); i++) {
+				if(haystack[i] == needle) return i;
+			}
+			return -1;
+		}
+
+		private int indexOf(char[] haystack, char needle, int fromIndexInclusive) {
+			for(int i = fromIndexInclusive; i < haystack.length; i++) {
+				if(haystack[i] == needle) return i;
+			}
+			return -1;
+		}
+
+		private int indexOf(char[] haystack, char[] needle, int fromIndexInclusive) {
+			final int end = haystack.length - needle.length;
+			for(int i = fromIndexInclusive; i <= end; i++) {
+				if(equals(haystack, needle, i)) return i;
+			}
+			return -1;
+		}
+
+		private boolean equals(char[] haystack, char[] needle, int haystackPos) {
+			for(int i = 0; i < needle.length; i++) {
+				if(haystack[haystackPos + i] != needle[i]) return false;
+			}
+			return true;
 		}
 	}
 

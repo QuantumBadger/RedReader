0581587154dfa6bd40ff28adb654362bbcc0a6d6
AlexStuckless <stuckless.a@gmail.com>
2014-11-20 10:07:15 -0800

    The comment does get upvoted/downvoted/unvoted, but it won't show on the screen. If you look at the same comment on the site, you'll see that it knows what you've swiped as. I'm guessing the comment needs some sort of refresh.

--- src/main/java/org/quantumbadger/redreader/views/RedditCommentView.java
+++ src/main/java/org/quantumbadger/redreader/views/RedditCommentView.java
@@ -148,11 +148,20 @@ public class RedditCommentView extends LinearLayout{
 					Log.d("RedditCommentView", "Must DOWNVOTE");
 					comment.action(fragment.getSupportActivity(), RedditAPI.RedditAction.DOWNVOTE);
 				}
+				comment.refreshView(context);
 			}
 
 			@Override
 			public void swipeRight() {
 				Log.d("RedditCommentView", "Swiped right");
+				if(comment.isUpvoted()){
+					Log.d("RedditCommentView", "Must UNVOTE");
+					comment.action(fragment.getSupportActivity(), RedditAPI.RedditAction.UNVOTE);
+				}else{
+					Log.d("RedditCommentView", "Must UPVOTE");
+					comment.action(fragment.getSupportActivity(), RedditAPI.RedditAction.UPVOTE);
+				}
+				comment.refreshView(context);
 			}
 		});
 	}

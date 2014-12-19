99c57148233352b178d615201715535357a4d955
QuantumBadger <git@redreader.org>
2014-12-19 19:18:36 +0000

    Don't unnecessarily reset RedditPostView

--- src/main/java/org/quantumbadger/redreader/views/RedditPostView.java
+++ src/main/java/org/quantumbadger/redreader/views/RedditPostView.java
@@ -298,6 +298,10 @@ public final class RedditPostView extends SwipableListItemView implements Reddit
 	// Only run in the UI thread
 	public void reset(final RedditPreparedPost data) {
 
+		if(post == data) {
+			return;
+		}
+
 		usageId++;
 
 		super.reset();

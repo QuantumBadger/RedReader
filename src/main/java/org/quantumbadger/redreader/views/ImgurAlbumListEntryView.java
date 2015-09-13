e72bdea6c75c99db181afa983d21ae093cdfe728
QuantumBadger <git@redreader.org>
2015-09-13 15:48:44 +0100

    Thumbnails for imgur albums

--- src/main/java/org/quantumbadger/redreader/views/ImgurAlbumListEntryView.java
+++ src/main/java/org/quantumbadger/redreader/views/ImgurAlbumListEntryView.java
@@ -2,14 +2,28 @@ package org.quantumbadger.redreader.views;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.Gravity;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import org.apache.http.StatusLine;
+import org.quantumbadger.redreader.account.RedditAccountManager;
+import org.quantumbadger.redreader.cache.CacheManager;
+import org.quantumbadger.redreader.cache.CacheRequest;
+import org.quantumbadger.redreader.cache.RequestFailureType;
+import org.quantumbadger.redreader.common.AndroidApi;
+import org.quantumbadger.redreader.common.Constants;
 import org.quantumbadger.redreader.common.General;
 import org.quantumbadger.redreader.image.ImgurAPI;
 
+import java.io.IOException;
+import java.util.UUID;
+
 public class ImgurAlbumListEntryView extends LinearLayout {
 
+	private final ImageView mThumbnail;
 	private final TextView mTitle;
 	private final TextView mSubtitle;
 
@@ -19,19 +33,33 @@ public class ImgurAlbumListEntryView extends LinearLayout {
 
 		super(context);
 
-		setOrientation(VERTICAL);
+		setOrientation(HORIZONTAL);
 
+		mThumbnail = new ImageView(context);
 		mTitle = new TextView(context);
 		mSubtitle = new TextView(context);
 
-		addView(mTitle);
-		addView(mSubtitle);
+		final LinearLayout textLayout = new LinearLayout(context);
+		textLayout.setOrientation(VERTICAL);
 
-		final int paddingSidesPx = General.dpToPixels(context, 10.0f);
+		textLayout.addView(mTitle);
+		textLayout.addView(mSubtitle);
+
+		addView(mThumbnail);
+		addView(textLayout);
+
+		textLayout.setGravity(Gravity.CENTER_VERTICAL);
+		setGravity(Gravity.CENTER_VERTICAL);
+
+		final int thumbnailSizePx = General.dpToPixels(context, 64);
+		mThumbnail.getLayoutParams().width = thumbnailSizePx;
+		mThumbnail.getLayoutParams().height = thumbnailSizePx;
+
+		final int paddingSidesPx = General.dpToPixels(context, 15.0f);
 		final int paddingTopBottomPx = General.dpToPixels(context, 10.0f);
 		final int paddingSeparationPx = General.dpToPixels(context, 3.0f);
 
-		setPadding(paddingSidesPx, paddingTopBottomPx, paddingSidesPx, paddingTopBottomPx);
+		textLayout.setPadding(paddingSidesPx, paddingTopBottomPx, paddingSidesPx, paddingTopBottomPx);
 		mSubtitle.setPadding(0, paddingSeparationPx, 0, 0);
 
 		mTitle.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
@@ -40,6 +68,8 @@ public class ImgurAlbumListEntryView extends LinearLayout {
 	}
 
 	public void reset(final int listPosition, final ImgurAPI.ImageInfo info) {
+
+		mImageInfo = info;
 
 		if(info.title == null || info.title.trim().isEmpty()) {
 			mTitle.setText("Image " + listPosition);
@@ -80,6 +110,60 @@ public class ImgurAlbumListEntryView extends LinearLayout {
 		}
 
 		mSubtitle.setText(subtitle);
+
+		mThumbnail.setImageBitmap(null);
+
+		// TODO check thumbnail preference
+		CacheManager.getInstance(getContext()).makeRequest(new CacheRequest(
+				General.uriFromString(info.urlBigSquare),
+				RedditAccountManager.getAnon(),
+				null,
+				Constants.Priority.THUMBNAIL,
+				listPosition,
+				CacheRequest.DownloadType.IF_NECESSARY,
+				Constants.FileType.THUMBNAIL,
+				false,
+				false,
+				false,
+				getContext()
+		) {
+			@Override
+			protected void onCallbackException(final Throwable t) {
+				Log.e("ImgurAlbumListEntryView", "Error in album thumbnail fetch callback", t);
+			}
+
+			@Override
+			protected void onDownloadNecessary() {}
+
+			@Override
+			protected void onDownloadStarted() {}
+
+			@Override
+			protected void onFailure(final RequestFailureType type, final Throwable t, final StatusLine status, final String readableMessage) {
+				Log.e("ImgurAlbumListEntryView", "Failed to fetch thumbnail " + url.toString());
+			}
+
+			@Override
+			protected void onProgress(final boolean authorizationInProgress, final long bytesRead, final long totalBytes) {}
+
+			@Override
+			protected void onSuccess(final CacheManager.ReadableCacheFile cacheFile, final long timestamp, final UUID session, final boolean fromCache, final String mimetype) {
+
+				if(mImageInfo != info) return;
+
+				// TODO post message rather than runnable
+				AndroidApi.UI_THREAD_HANDLER.post(new Runnable() {
+					@Override
+					public void run() {
+						try {
+							mThumbnail.setImageURI(cacheFile.getUri());
+						} catch(IOException e) {
+							throw new RuntimeException(e);
+						}
+					}
+				});
+			}
+		});
 	}
 
 

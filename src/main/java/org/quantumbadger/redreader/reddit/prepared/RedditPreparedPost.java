782a312d8f4d2dce86e58a08bc7a1b9f754d37b5
fhtagn <sergio.a.sampaio@gmail.com>
2016-12-13 21:07:15 +0100

    Saving file to storage before creating Intent
    Even with intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION),
    Whatsapp wouldn't accept the intent. Maybe they ignore mimetype
    and only look into file extension?
    
    This does the same as "Save image", but then shares said file.
    Multiple shares of the same file will repeatedly save the file.

--- src/main/java/org/quantumbadger/redreader/reddit/prepared/RedditPreparedPost.java
+++ src/main/java/org/quantumbadger/redreader/reddit/prepared/RedditPreparedPost.java
@@ -518,102 +518,134 @@ public final class RedditPreparedPost {
 				break;
 			}
 
-      		case SHARE_IMAGE: {
+			case SHARE_IMAGE: {
 
-				final RedditAccount anon = RedditAccountManager.getAnon();
+				((BaseActivity)activity).requestPermissionWithCallback(Manifest.permission.WRITE_EXTERNAL_STORAGE, new BaseActivity.PermissionCallback() {
+					@Override
+					public void onPermissionGranted() {
 
-				LinkHandler.getImageInfo(activity, post.src.getUrl(), Constants.Priority.IMAGE_VIEW, 0, new GetImageInfoListener() {
+						final RedditAccount anon = RedditAccountManager.getAnon();
 
-				  @Override
-				  public void onFailure(final @CacheRequest.RequestFailureType int type, final Throwable t, final Integer status, final String readableMessage) {
-					final RRError error = General.getGeneralErrorForFailure(activity, type, t, status, post.src.getUrl());
-					General.showResultDialog(activity, error);
-				  }
+						LinkHandler.getImageInfo(activity, post.src.getUrl(), Constants.Priority.IMAGE_VIEW, 0, new GetImageInfoListener() {
 
-				  @Override
-				  public void onSuccess(final ImageInfo info) {
+							@Override
+							public void onFailure(final @CacheRequest.RequestFailureType int type, final Throwable t, final Integer status, final String readableMessage) {
+								final RRError error = General.getGeneralErrorForFailure(activity, type, t, status, post.src.getUrl());
+								General.showResultDialog(activity, error);
+							}
 
-					CacheManager.getInstance(activity).makeRequest(new CacheRequest(
-						General.uriFromString(info.urlOriginal),
-						anon,
-						null,
-						Constants.Priority.IMAGE_VIEW,
-						0,
-						DownloadStrategyIfNotCached.INSTANCE,
-						Constants.FileType.IMAGE,
-						CacheRequest.DOWNLOAD_QUEUE_IMMEDIATE,
-						false,
-						false,
-						activity) {
+							@Override
+							public void onSuccess(final ImageInfo info) {
 
-					  @Override
-					  protected void onCallbackException(Throwable t) {
-						BugReportActivity.handleGlobalError(context, t);
-					  }
+								CacheManager.getInstance(activity).makeRequest(new CacheRequest(
+										General.uriFromString(info.urlOriginal),
+										anon,
+										null,
+										Constants.Priority.IMAGE_VIEW,
+										0,
+										DownloadStrategyIfNotCached.INSTANCE,
+										Constants.FileType.IMAGE,
+										CacheRequest.DOWNLOAD_QUEUE_IMMEDIATE,
+										false,
+										false,
+										activity) {
 
-					  @Override
-					  protected void onDownloadNecessary() {
-						General.quickToast(context, R.string.download_downloading);
-					  }
+									@Override
+									protected void onCallbackException(Throwable t) {
+										BugReportActivity.handleGlobalError(context, t);
+									}
 
-					  @Override
-					  protected void onDownloadStarted() {
-					  }
+									@Override
+									protected void onDownloadNecessary() {
+										General.quickToast(context, R.string.download_downloading);
+									}
 
-					  @Override
-					  protected void onFailure(
-						  @CacheRequest.RequestFailureType int type,
-						  Throwable t,
-						  Integer status,
-						  String readableMessage) {
+									@Override
+									protected void onDownloadStarted() {
+									}
 
-						final RRError error = General.getGeneralErrorForFailure(context, type, t, status, url.toString());
-						General.showResultDialog(activity, error);
-					  }
+									@Override
+									protected void onFailure(
+											@CacheRequest.RequestFailureType int type,
+											Throwable t,
+											Integer status,
+											String readableMessage) {
 
-					  @Override
-					  protected void onProgress(
-						  boolean authorizationInProgress,
-						  long bytesRead,
-						  long totalBytes) {
-					  }
+										final RRError error = General.getGeneralErrorForFailure(context, type, t, status, url.toString());
+										General.showResultDialog(activity, error);
+									}
 
-					  @Override
-					  protected void onSuccess(
-						  CacheManager.ReadableCacheFile cacheFile,
-						  long timestamp,
-						  UUID session,
-						  boolean fromCache,
-						  String mimetype) {
+									@Override
+									protected void onProgress(
+											boolean authorizationInProgress,
+											long bytesRead,
+											long totalBytes) {
+									}
 
-						try {
-						  final InputStream cacheFileInputStream = cacheFile.getInputStream();
+									@Override
+									protected void onSuccess(
+											CacheManager.ReadableCacheFile cacheFile,
+											long timestamp,
+											UUID session,
+											boolean fromCache,
+											String mimetype) {
 
-						  if(cacheFileInputStream == null) {
-							notifyFailure(CacheRequest.REQUEST_FAILURE_CACHE_MISS, null, null, "Could not find cached image");
-							return;
-						  }
+										String filename = General.filenameFromString(info.urlOriginal);
+										File dst = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), filename);
 
-						  Intent shareIntent = new Intent();
-						  shareIntent.setAction(Intent.ACTION_SEND);
-						  shareIntent.putExtra(Intent.EXTRA_STREAM, cacheFile.getUri());
-						  shareIntent.setType(mimetype);
-						  activity.startActivity(Intent.createChooser(shareIntent, activity.getString(R.string.action_share_image)));
+										if(dst.exists()) {
+											int count = 0;
 
-						} catch(IOException e) {
-						  notifyFailure(CacheRequest.REQUEST_FAILURE_STORAGE, e, null, "Could not copy file");
-						  return;
-						}
+											while(dst.exists()) {
+												count++;
+												dst = new File(
+														Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+														count + "_" + filename.substring(1));
+											}
+										}
 
-					  }
-					});
+										try {
+											final InputStream cacheFileInputStream = cacheFile.getInputStream();
 
-				  }
+											if(cacheFileInputStream == null) {
+												notifyFailure(CacheRequest.REQUEST_FAILURE_CACHE_MISS, null, null, "Could not find cached image");
+												return;
+											}
 
-				  @Override
-				  public void onNotAnImage() {
-					General.quickToast(activity, R.string.selected_link_is_not_image);
-				  }
+											General.copyFile(cacheFileInputStream, dst);
+
+											Intent shareIntent = new Intent();
+											shareIntent.setAction(Intent.ACTION_SEND);
+											shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + dst.getAbsolutePath()));
+											shareIntent.setType(mimetype);
+											activity.startActivity(Intent.createChooser(shareIntent, activity.getString(R.string.action_share_image)));
+
+										} catch(IOException e) {
+											notifyFailure(CacheRequest.REQUEST_FAILURE_STORAGE, e, null, "Could not copy file");
+											return;
+										}
+
+										activity.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+												Uri.parse("file://" + dst.getAbsolutePath()))
+										);
+
+										General.quickToast(context, context.getString(R.string.action_save_image_success) + " " + dst.getAbsolutePath());
+									}
+								});
+
+							}
+
+							@Override
+							public void onNotAnImage() {
+								General.quickToast(activity, R.string.selected_link_is_not_image);
+							}
+						});
+					}
+
+					@Override
+					public void onPermissionDenied() {
+						General.quickToast(activity, R.string.save_image_permission_denied);
+					}
 				});
 
 				break;

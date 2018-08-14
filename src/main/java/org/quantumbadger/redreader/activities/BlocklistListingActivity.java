3bd7a393b2ede364cda2a88f51c06da9fb2db363
Carsten <carsten@kopis.de>
2018-08-14 10:44:50 +0200

    WIP Try to use ActionBar
    I moved from ListActivity to BaseActivity to re-use the ActionBar methods,
    but now the list is not updated after I added a new blocklist entry. I
    couldn't figure out how to update the ListView - I think it has something
    to do with the wrapper that's added for the ActionBar layout.

--- src/main/java/org/quantumbadger/redreader/activities/BlocklistListingActivity.java
+++ src/main/java/org/quantumbadger/redreader/activities/BlocklistListingActivity.java
@@ -1,34 +1,50 @@
 package org.quantumbadger.redreader.activities;
 
 import android.app.AlertDialog;
-import android.app.ListActivity;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.*;
 import org.quantumbadger.redreader.R;
 import org.quantumbadger.redreader.common.PrefsUtility;
 
 import java.util.List;
 
-public class BlocklistListingActivity extends ListActivity {
+public class BlocklistListingActivity extends BaseActivity {
 	private static final String TAG = BlocklistListingActivity.class.getSimpleName();
 
 	private List<String> blockedPostUrls;
 
+	protected ListAdapter mAdapter;
+	protected ListView mList;
+
+	private Handler mHandler = new Handler();
+	private boolean mFinishedStart = false;
+
+	private Runnable mRequestFocus = new Runnable() {
+		public void run() {
+			mList.focusableViewAvailable(mList);
+		}
+	};
+
+	private AdapterView.OnItemClickListener mOnClickListener = new AdapterView.OnItemClickListener() {
+		public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+			onListItemClick((ListView) parent, v, position, id);
+		}
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-
 		PrefsUtility.applyTheme(this);
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.blocklist_list);
+		setBaseActivityContentView(R.layout.blocklist_list);
+		refresh();
 
 		final SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 		blockedPostUrls = PrefsUtility.pref_blocked_post_urls(this, sharedPreferences);
@@ -43,9 +59,9 @@ public class BlocklistListingActivity extends ListActivity {
 		urlEditView.setInputType(InputType.TYPE_CLASS_TEXT);
 
 		final AlertDialog dialog = new AlertDialog.Builder(this)
-				.setTitle("Enter URL")
+				.setTitle(R.string.pref_blocked_post_enter_domain)
 				.setView(urlEditView)
-				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+				.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						final String url = urlEditView.getText().toString();
@@ -57,7 +73,7 @@ public class BlocklistListingActivity extends ListActivity {
 						BlocklistListingActivity.this.onContentChanged();
 					}
 				})
-				.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						dialog.cancel();
@@ -68,6 +84,46 @@ public class BlocklistListingActivity extends ListActivity {
 	}
 
 	@Override
+	protected void onRestoreInstanceState(Bundle state) {
+		ensureList();
+		super.onRestoreInstanceState(state);
+	}
+
+	@Override
+	protected void onDestroy() {
+		mHandler.removeCallbacks(mRequestFocus);
+		super.onDestroy();
+	}
+
+	private void refresh() {
+		super.onContentChanged();
+		final View baseView = getLayoutInflater().inflate(R.layout.blocklist_list, null);
+		final View emptyView = baseView.findViewById(android.R.id.empty);
+		mList = (ListView) baseView.findViewById(R.id.blocklist_list);
+		if (mList == null) {
+			throw new RuntimeException(
+					"Your content must have a ListView whose id attribute is " +
+							"'blocklist_list'");
+		}
+		if (emptyView != null) {
+			mList.setEmptyView(emptyView);
+		}
+		mList.setOnItemClickListener(mOnClickListener);
+		if (mFinishedStart) {
+			setListAdapter(mAdapter);
+		}
+		mHandler.post(mRequestFocus);
+		mFinishedStart = true;
+	}
+
+	public void setListAdapter(ListAdapter adapter) {
+		synchronized (this) {
+			ensureList();
+			mAdapter = adapter;
+			mList.setAdapter(adapter);
+		}
+	}
+
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		final String url = (String) getListAdapter().getItem(position);
 		Log.d(TAG, "Removed: " + url);
@@ -77,5 +133,33 @@ public class BlocklistListingActivity extends ListActivity {
 
 		blockedPostUrls.remove(url);
 		this.onContentChanged();
+	}
+
+	public void setSelection(int position) {
+		mList.setSelection(position);
+	}
+
+	public int getSelectedItemPosition() {
+		return mList.getSelectedItemPosition();
+	}
+
+	public long getSelectedItemId() {
+		return mList.getSelectedItemId();
+	}
+
+	public ListView getListView() {
+		ensureList();
+		return mList;
+	}
+
+	public ListAdapter getListAdapter() {
+		return mAdapter;
+	}
+
+	private void ensureList() {
+		if (mList != null) {
+			return;
+		}
+		setContentView(android.R.layout.list_content);
 	}
 }

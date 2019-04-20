6379e6d9b1808c6967cae9a1af3b531008b58805
Jantz <jantzillac.93@gmail.com>
2019-04-20 15:23:04 -0500

    Add new "Navigation" preference to settings

--- src/main/java/org/quantumbadger/redreader/settings/SettingsFragment.java
+++ src/main/java/org/quantumbadger/redreader/settings/SettingsFragment.java
@@ -76,6 +76,7 @@ public final class SettingsFragment extends PreferenceFragment {
 				R.string.pref_behaviour_fling_comment_left_key,
 				R.string.pref_behaviour_fling_comment_right_key,
 				R.string.pref_appearance_theme_key,
+				R.string.pref_appearance_navigate_key,
 				R.string.pref_appearance_navbar_color_key,
 				R.string.pref_cache_maxage_listing_key,
 				R.string.pref_cache_maxage_thumb_key,

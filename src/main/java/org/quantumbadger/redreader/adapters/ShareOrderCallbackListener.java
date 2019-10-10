71f7bea349a895e4b11a4a0bb13e256363e3158a
Martin Betcher <martin.betcher@gmail.com>
2019-10-10 11:58:43 +0200

    deleted unnecessary enums in ShareOrderCallbackListener

--- src/main/java/org/quantumbadger/redreader/adapters/ShareOrderCallbackListener.java
+++ src/main/java/org/quantumbadger/redreader/adapters/ShareOrderCallbackListener.java
@@ -1,13 +1,6 @@
 package org.quantumbadger.redreader.adapters;
 
 public interface ShareOrderCallbackListener {
-
-	enum MediaType{
-		PLAINTEXT,
-		IMAGE,
-		VIDEO
-	}
-
 	void onSelectedIntent(int position);
 
 }

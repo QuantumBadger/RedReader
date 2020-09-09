5faf259af74854b36ea1dc5666d368c326bc4c81
QuantumBadger <git@redreader.org>
2020-09-09 19:58:01 +0100

    Initial UI tests

---
+++ src/androidTest/java/org/quantumbadger/redreader/activities/BasicUITest.java
@@ -0,0 +1,130 @@
+/*******************************************************************************
+ * This file is part of RedReader.
+ *
+ * RedReader is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * RedReader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RedReader.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package org.quantumbadger.redreader.activities;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.quantumbadger.redreader.R;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class BasicUITest {
+
+	@Rule
+	public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+	@Test
+	public void basicUITest() {
+		onView(allOf(
+				withId(android.R.id.button1),
+				withText("Log in now"),
+				childAtPosition(
+						childAtPosition(withClassName(is("android.widget.ScrollView")), 0),
+						3)))
+				.perform(scrollTo(), click());
+
+		onView(allOf(
+				withId(android.R.id.button3),
+				withText("Close"),
+				childAtPosition(
+						childAtPosition(
+								withClassName(is("android.widget.ScrollView")),
+								0),
+						0)))
+				.perform(scrollTo(), click());
+
+		onView(allOf(
+				withContentDescription("More options"),
+				childAtPosition(
+						childAtPosition(
+								withId(R.id.rr_actionbar_toolbar),
+								1),
+						1),
+				isDisplayed()))
+				.perform(click());
+
+		onView(allOf(
+				withId(R.id.title),
+				withText("Settings"),
+				childAtPosition(
+						childAtPosition(
+								withId(R.id.content),
+								0),
+						0),
+				isDisplayed()))
+				.perform(click());
+
+		onView(allOf(
+				withContentDescription("Navigate up"),
+				childAtPosition(
+						allOf(withId(R.id.action_bar),
+								childAtPosition(
+										withId(R.id.action_bar_container),
+										0)),
+						1),
+				isDisplayed()))
+				.perform(click());
+
+		onView(withId(R.id.actionbar_title_text))
+				.check(matches(allOf(withText("RedReader"), isDisplayed())));
+	}
+
+	private static Matcher<View> childAtPosition(
+			final Matcher<View> parentMatcher, final int position) {
+
+		return new TypeSafeMatcher<View>() {
+			@Override
+			public void describeTo(Description description) {
+				description.appendText("Child at position " + position + " in parent ");
+				parentMatcher.describeTo(description);
+			}
+
+			@Override
+			public boolean matchesSafely(View view) {
+				ViewParent parent = view.getParent();
+				return parent instanceof ViewGroup && parentMatcher.matches(parent)
+						&& view.equals(((ViewGroup)parent).getChildAt(position));
+			}
+		};
+	}
+}

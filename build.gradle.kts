7c6eebfd0fbc806c205636bd71464da7122c76ea
QuantumBadger <git@redreader.org>
2023-12-17 18:10:53 +0000

    Updating version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -73,8 +73,8 @@ android {
 		applicationId = "org.quantumbadger.redreader"
 		minSdk = 16
 		targetSdk = 33
-		versionCode = 108
-		versionName = "1.22"
+		versionCode = 109
+		versionName = "1.23"
 
 		multiDexEnabled = true
 		vectorDrawables.generatedDensities("mdpi", "hdpi", "xhdpi", "xxhdpi", "xxxhdpi")

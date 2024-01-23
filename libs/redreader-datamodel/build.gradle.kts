b9ae9a184a43a061abba09ca3005128a878af59f
Alexey Rochev <equeim@gmail.com>
2024-01-24 00:25:08 +0300

    Update Kotlin to 1.9.22

--- libs/redreader-datamodel/build.gradle.kts
+++ libs/redreader-datamodel/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
@@ -6,6 +8,10 @@ plugins {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType(KotlinJvmCompile::class) {
+    kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
 }
 
 dependencies {

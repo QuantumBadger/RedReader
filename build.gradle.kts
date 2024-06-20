6b4f359a08aa542354bfe175d106599de9b8f968
QuantumBadger <git@redreader.org>
2024-06-20 11:56:19 +0100

    Add Jetpack Compose dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,6 +52,13 @@ dependencies {
 		}
 	}
 
+	implementation(platform(libs.androidx.compose.bom))
+	implementation(libs.androidx.compose.material)
+	implementation(libs.androidx.compose.runtime)
+	implementation(libs.androidx.compose.ui)
+	implementation(libs.androidx.compose.ui.graphics)
+	implementation(libs.androidx.compose.ui.tooling)
+
 	testImplementation(libs.junit)
 
 	androidTestImplementation(libs.androidx.test.core)

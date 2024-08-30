a4d6839bdc9a2d6b2f372dcb7ddb6bcbdaf8198f
Eric Cawthorne <eccawthorne@gmail.com>
2024-08-30 13:35:37 -0400

    Change seek position calculation to use video framerate

--- src/main/java/org/quantumbadger/redreader/views/video/ExoPlayerWrapperView.java
+++ src/main/java/org/quantumbadger/redreader/views/video/ExoPlayerWrapperView.java
@@ -39,6 +39,7 @@ import androidx.media3.common.PlaybackException;
 import androidx.media3.common.Player;
 import androidx.media3.common.util.UnstableApi;
 import androidx.media3.exoplayer.ExoPlayer;
+import androidx.media3.exoplayer.SeekParameters;
 import androidx.media3.exoplayer.source.MediaSource;
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector;
 import androidx.media3.ui.AspectRatioFrameLayout;
@@ -186,7 +187,8 @@ public class ExoPlayerWrapperView extends FrameLayout {
 						R.drawable.icon_step_back,
 						R.string.video_step_back,
 						view -> {
-							mVideoPlayer.seekTo(mVideoPlayer.getCurrentPosition() - 33);
+							long frameRate = mVideoPlayer.getVideoFormat() != null ?  30 : (long) mVideoPlayer.getVideoFormat().frameRate;
+							mVideoPlayer.seekTo(mVideoPlayer.getCurrentPosition() - frameRate);
 							updateProgress();
 						}
 				));
@@ -197,7 +199,8 @@ public class ExoPlayerWrapperView extends FrameLayout {
 						R.drawable.icon_step_forward,
 						R.string.video_step_forward,
 						view -> {
-							mVideoPlayer.seekTo(mVideoPlayer.getCurrentPosition() + 33);
+							long frameRate = mVideoPlayer.getVideoFormat() != null ?  30 : (long) mVideoPlayer.getVideoFormat().frameRate;
+							mVideoPlayer.seekTo(mVideoPlayer.getCurrentPosition() + frameRate);
 							updateProgress();
 						}
 				));

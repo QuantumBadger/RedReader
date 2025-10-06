4f33d0d00d753095f55b642b3870132423a7e56d
Blue Beam Dev <jonathanp@tuta.io>
2025-10-06 16:02:15 -0400

    fixed rules from pull request ci

--- src/main/java/org/quantumbadger/redreader/activities/OAuthLoginActivity.java
+++ src/main/java/org/quantumbadger/redreader/activities/OAuthLoginActivity.java
@@ -1,20 +1,3 @@
-/*******************************************************************************
- * This file is part of RedReader.
- *
- * RedReader is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * RedReader is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with RedReader.  If not, see <http://www.gnu.org/licenses/>.
- ******************************************************************************/
-
 package org.quantumbadger.redreader.activities;
 
 import android.annotation.SuppressLint;
@@ -29,16 +12,17 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
 import androidx.webkit.WebSettingsCompat;
 import androidx.webkit.WebViewFeature;
-import java.util.Map;
+
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import android.os.Message;
 import android.net.http.SslError;
 import android.webkit.SslErrorHandler;
-
-import androidx.browser.customtabs.CustomTabsIntent;
-
 
 import org.quantumbadger.redreader.R;
 import org.quantumbadger.redreader.RedReader;
@@ -46,15 +30,11 @@ import org.quantumbadger.redreader.common.PrefsUtility;
 import org.quantumbadger.redreader.common.TorCommon;
 import org.quantumbadger.redreader.reddit.api.RedditOAuth;
 
-import java.util.Objects;
-
 import info.guardianproject.netcipher.webkit.WebkitProxy;
 
 public class OAuthLoginActivity extends ViewsBaseActivity {
 
-	private static final String OAUTH_HOST = "rr_oauth_redir";
 	private static final String REDREADER_SCHEME = "redreader";
-	private static final String HTTP_SCHEME = "http";
 
 	private WebView mWebView;
 
@@ -89,13 +69,13 @@ public class OAuthLoginActivity extends ViewsBaseActivity {
 							getResources().getString(R.string.error_tor_setting_failed)
 					);
 				}
-			} catch (Throwable t) {
+			} catch (final Throwable t) {
 				BugReportActivity.handleGlobalError(this, t);
 			}
 		}
 
 		// 1) Cookies & storage must be fully enabled
-		CookieManager cookieMgr = CookieManager.getInstance();
+		final CookieManager cookieMgr = CookieManager.getInstance();
 		cookieMgr.setAcceptCookie(true);
 		// third-party cookies are required for many IdP flows
 		CookieManager.getInstance().setAcceptThirdPartyCookies(mWebView, true);
@@ -114,7 +94,7 @@ public class OAuthLoginActivity extends ViewsBaseActivity {
 		if (WebViewFeature.isFeatureSupported(WebViewFeature.SAFE_BROWSING_ENABLE)) {
 			try {
 				WebSettingsCompat.setSafeBrowsingEnabled(settings, true);
-			} catch (Throwable ignored) { /* no-op */ }
+			} catch (final Throwable ignored) { /* no-op */ }
 		}
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 			settings.setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
@@ -127,33 +107,36 @@ public class OAuthLoginActivity extends ViewsBaseActivity {
 						+ "Chrome/124.0.0.0 Mobile Safari/537.36"
 		);
 
-		// Remove/neutralize the X-Requested-With header (WebView adds it by default);
-
-		final Map<String,String> firstLoadHeaders = new HashMap<>();
+		// Remove/neutralize the X-Requested-With header (WebView adds it by default)
+		final Map<String, String> firstLoadHeaders = new HashMap<>();
 		firstLoadHeaders.put("X-Requested-With", ""); // override to empty
 
 		mWebView.setWebChromeClient(new WebChromeClient() {
-			@Override public boolean onCreateWindow(WebView view, boolean isDialog,
-													boolean isUserGesture, Message resultMsg) {
+			@Override
+			public boolean onCreateWindow(final WebView view, final boolean isDialog,
+										  final boolean isUserGesture, final Message resultMsg) {
 				// open popup targets inside the same webview
-				WebView.HitTestResult result = view.getHitTestResult();
-				WebView newWebView = new WebView(view.getContext());
+				final WebView newWebView = new WebView(view.getContext());
 				newWebView.setWebViewClient(new WebViewClient());
 				((WebView.WebViewTransport) resultMsg.obj).setWebView(newWebView);
 				resultMsg.sendToTarget();
 				return true;
 			}
-			@Override public boolean onConsoleMessage(ConsoleMessage consoleMessage) { return true; }
+
+			@Override
+			public boolean onConsoleMessage(final ConsoleMessage consoleMessage) {
+				return true;
+			}
 		});
 
 		mWebView.setWebViewClient(new WebViewClient() {
 			@Override
-			public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest req) {
-				Uri url = req.getUrl();
+			public boolean shouldOverrideUrlLoading(final WebView view, final WebResourceRequest req) {
+				final Uri url = req.getUrl();
 
 				// intercept the oauth redirect back to the app
-				if (Objects.equals(url.getScheme(), "redreader")) {
-					Intent intent = new Intent();
+				if (Objects.equals(url.getScheme(), REDREADER_SCHEME)) {
+					final Intent intent = new Intent();
 					intent.putExtra("url", url.toString());
 					setResult(123, intent);
 					finish();
@@ -161,14 +144,14 @@ public class OAuthLoginActivity extends ViewsBaseActivity {
 				}
 
 				// keep everything else inside, but scrub X-Requested-With on every nav
-				Map<String,String> headers = new HashMap<>();
+				final Map<String, String> headers = new HashMap<>();
 				headers.put("X-Requested-With", "");
 				view.loadUrl(url.toString(), headers);
 				return true; // we handled it
 			}
 
 			@Override
-			public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+			public void onReceivedSslError(final WebView view, final SslErrorHandler handler, final SslError error) {
 				// be strict; do not proceed on SSL errors
 				handler.cancel();
 			}
@@ -180,11 +163,8 @@ public class OAuthLoginActivity extends ViewsBaseActivity {
 		mWebView.loadUrl(RedditOAuth.getPromptUri().toString(), firstLoadHeaders);
 	}
 
-
-
 	@Override
 	protected void onPause() {
-
 		super.onPause();
 
 		if (mWebView != null) {
